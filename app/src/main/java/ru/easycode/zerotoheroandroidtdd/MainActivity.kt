23fa99b300a5b1fd9ea0c8e0db4cf9417502e623
nellisheg <zelenyy.grib@mail.ru>
2024-04-18 14:48:32 +0300

    9 pre-solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,39 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView: TextView
+    private lateinit var button: Button
+    private lateinit var layout: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.removeButton)
+        textView = findViewById(R.id.titleTextView)
+        layout = findViewById(R.id.rootLayout)
+
+        button.setOnClickListener {  layout.removeView(textView) }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY,layout.removeView(textView).toInt() )
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+    }
+
+
+    companion object{
+        const val KEY = "viewGone"
     }
 }
